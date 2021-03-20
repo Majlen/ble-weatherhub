aa7ee969a933752caced858a6a4b50b3733b8b10
Milan Ševčík <milansevcik@fb.com>
2021-03-20 23:14:23 +0000

    Add Influx bucket and org to config

--- src/main/java/cz/majlen/weather_ble/config/InfluxConfig.java
+++ src/main/java/cz/majlen/weather_ble/config/InfluxConfig.java
@@ -3,6 +3,8 @@ package cz.majlen.weather_ble.config;
 public class InfluxConfig {
 	private String connection;
 	private String token;
+	private String bucket;
+	private String org;
 	
 	public String getConnection() {
 		return connection;
@@ -18,5 +20,21 @@ public class InfluxConfig {
 	
 	public void setToken(String token) {
 		this.token = token;
+	}
+	
+	public String getBucket() {
+		return bucket;
+	}
+	
+	public void setBucket(String bucket) {
+		this.bucket = bucket;
+	}
+	
+	public String getOrg() {
+		return org;
+	}
+	
+	public void setOrg(String org) {
+		this.org = org;
 	}
 }
